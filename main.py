50587d75b5ba4db14d2b7163856f3aabf7a57df9
Partyschnitzel <96427927+Partyschnitzel@users.noreply.github.com>
2025-04-11 15:47:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ async def send_session_update(openai_ws):
       "session": {
           "turn_detection": {"type": "server_vad"},
           "input_audio_format": "g711_ulaw",
-          "output_audio_format": "g711_alaw",
+          "output_audio_format": "g711_ulaw",
           "voice": VOICE,
           "instructions": SYSTEM_MESSAGE,
           "modalities": ["audio"],
